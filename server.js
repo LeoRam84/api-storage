d3ae6656100b1cb126026ab6e1189dfce1c105ee
Leonardo Ramidan <leonardoramidan@hotmail.com>
2024-02-28 16:28:14 -0300

    Quinta Versão GitHub

--- server.js
+++ server.js
@@ -8,25 +8,15 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-
-// Configurar CORS para permitir solicitações de qualquer origem
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Permitir solicitações de qualquer origem
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 server.use(
  // Add custom route here if needed
  jsonServer.rewriter({
   "/*": "/$1",
  })
 );
-
 server.use(router);
 // Listen to port
-server.listen(process.env.PORT || 3000, () => {
+server.listen(3000, () => {
  console.log("JSON Server is running");
 });
 
