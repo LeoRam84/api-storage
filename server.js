9608f6ee04d251cb655b8302cdadc12032810330
Leonardo Ramidan <leonardoramidan@hotmail.com>
2024-02-28 13:12:12 -0300

    Terceira Versão GitHub

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use(
 );
 server.use(router);
 // Listen to port
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
  console.log("JSON Server is running");
 });
 
